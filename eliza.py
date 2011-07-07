2943864011e0b9602e2aa69f42f8591237245304
Ali Asad Lotia <lotia@umich.edu>
2011-07-07 21:48:02 +0100

    Alternatives.

--- eliza.py
+++ eliza.py
@@ -9,26 +9,35 @@ INITIAL_PROMPT = 'How are you feeling?'
 
 class Responses:
     def response_stock(parts):
-        return "How do you feel about that" 
+        return random.choice(["How do you feel about that?", "What's your favourite animal?", "Tell me about your mother?"])
 
     def response_noun1(parts):
+        responses = ["Why do you like %s?", "What do you like most about %s?", "Tell me more about %s?"]
         if 'NN' in parts:
-            return "Why do you like %s?" % random.choice(parts['NN'])
+            return random.choice(responses) % random.choice(parts['NN'])
+
+    def response_nouns2(parts):
+        if 'NN' in parts:
+            noun = random.choice(parts['NN'])
+            return "%s, %s, %s! Can you talk about something else please!" % (noun, noun.title(), noun.upper())
 
     def response_nouns1(parts):
+        responses = ["Tell me how %s make you feel?"]
         if 'NNS' in parts:
-            return "Tell me how %s make you feel?" % random.choice(parts['NNS'])
+            return random.choice(responses) % random.choice(parts['NNS'])
 
     def response_verb1(parts):
         if 'VB' in parts:
             verb = random.choice(parts['VB'])
             day = random.choice('Mondays Wednesdays Toast Acid'.split())
             return "Wow, I love to %s too, especially on %s. When do you like to %s?" % (verb, day, verb)
-        else: return "That's kind of dull. Tell me about your Mother?"            
 
 output = INITIAL_PROMPT
 while True:
     input = raw_input(PROMPT + output + "\n")
+    if input == "goodbye":
+        print "Fine...seeyas!"
+        sys.exit(0)
     parts = get_parts(input)
     funcs = [f for (n, f) in Responses.__dict__.items() if callable(f)]
     while True:
