f45693e7f06a60eaf273698550e613e33eb8b212
TomV <tom@viner.tv>
2011-07-07 20:45:42 +0100

    initial parsing with nltk

---
+++ parse.py
@@ -0,0 +1,15 @@
+import nltk
+
+def get_parts(text):
+    text = nltk.word_tokenize(text)
+    parts = nltk.pos_tag(text)
+    dic = {}
+    for work, part in parts:
+        if part in dic:
+            dic[part].append(work)
+        else:
+            dic[part] = [work]
+    return dic
+
+get_parts("hi how are you")
+    
